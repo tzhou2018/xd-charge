9b1f76382da4d5238066b507803456ec60c6b9ca
hoooxz <hooooxz@gmail.com>
2018-12-14 14:30:00 +0800

    add a notice of querying balance

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -59,6 +59,7 @@ Page({
     wx.showModal({
       title: '删除电费账号',
       content: `${code.addr}(${code.chargeCode})`,
+      confirmColor: '#45A8F4',
       success (res) {
         if(res.cancel) return;
         let result = delCodeInStor(code_id)
@@ -81,5 +82,33 @@ Page({
 
     
   },
+
+  /** 查询电费网址提示 */
+  onQueryBanlance () {
+    wx.showModal({
+      title: 'http://10.168.55.50:8088',
+      content: '请在校园网环境下访问, 默认密码为123456或6个0.',
+      cancelText: '知道了',
+      confirmText: '复制网址',
+      confirmColor: '#45A8F4',
+      success (res) {
+        if (!res.confirm) {
+          return;
+        }
+        wx.setClipboardData({
+          data: 'http://10.168.55.50:8088',
+          success (res) {
+            wx.showToast({
+              title: '网址已复制',
+              icon: 'none'
+            })
+            setTimeout(() => (
+              wx.hideToast()
+            ), 1500)
+          }
+        })
+      }
+    });
+  }
 
 })
