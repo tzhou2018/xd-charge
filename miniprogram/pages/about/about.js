3d153e43101160242a348774f2b73cd5a0ec6c06
hoooxz <hooooxz@gmail.com>
2019-04-23 22:36:38 +0800

    add url of the repo

--- miniprogram/pages/about/about.js
+++ miniprogram/pages/about/about.js
@@ -20,4 +20,20 @@ Page({
     })
   },
 
+  /** 将开源仓库地址复制到剪切板 */
+  bindCopy: function(e) {
+    wx.setClipboardData({
+      data: 'https://github.com/hoooxz/xd-charge',
+      success (res) {
+        wx.showToast({
+          title: '开源仓库地址已复制',
+          icon: 'none'
+        })
+        setTimeout(() => (
+          wx.hideToast()
+        ), 1500)
+      }
+    })
+  },
+
 })
